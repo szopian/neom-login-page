36b9226379343964062be9ec728166647cdc0429
szopian <szopiangreg@gmail.com>
2021-10-01 00:48:51 +0100

    add styles and componet form

--- login-page/src/components/form/form.jsx
+++ login-page/src/components/form/form.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 import "./form.scss";
 
-const Form = () => <div className="form"></div>;
+const Form = () => (
+  <div className="form">
+    <h1 className="header">Login Here</h1>
+    <label className="label">Username</label>
+    <input className="input" type="text" placeholder="Email or Phone" />
+    <label className="label">Password</label>
+    <input className="input" type="password" placeholder="Password" />
+  </div>
+);
 
 export default Form;
